31e3c62bce9fa064a6d7df2c29cb6726a24e7d62
Itzli Molina <itzli2000@msn.com>
2025-05-29 18:02:23 -0600

    chore(DesignTokens): update version to 0.0.6 and remove tslib dependency
    - Incremented version in package.json to 0.0.6.
    - Removed tslib as a dependency from pnpm-lock.yaml.
    - Updated eslint.config.mjs to ignore tslib in dependency checks.

--- packages/libs/design-tokens/eslint.config.mjs
+++ packages/libs/design-tokens/eslint.config.mjs
@@ -8,6 +8,7 @@ export default [
       '@nx/dependency-checks': [
         'error',
         {
+          ignoredDependencies: ['tslib'],
           ignoredFiles: ['{projectRoot}/eslint.config.{js,cjs,mjs}'],
         },
       ],
